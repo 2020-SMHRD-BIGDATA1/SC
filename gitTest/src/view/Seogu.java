2a6e1c3809e2206f52e3e377141939d14cceec4a
SMHRD <SMHRD@DESKTOP-GQPM9BF>
2020-06-24 17:54:47 +0900

    1. 광주광역시 CCTV 현황 GUI구현 2. 광주광역시 CCTV 현황 클래스 생성 3. 서구 제외 이미지 사진 추가

--- gitTest/src/view/Seogu.java
+++ gitTest/src/view/Seogu.java
@@ -4,77 +4,93 @@ import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class Seogu extends JFrame {
-   Container contentPane;
-   String[] gu = { "광천동", "금호동","내방동", "농성동", "덕흥동", "동천동", "마륵동", "매월동", 
-		   "벽진동","서창동", "세하동", "쌍촌동", "양동", "용두동", 
-		   "유촌동", "치평동", "풍암동", "화정동"};
-   
-   String pathgc = this.getClass().getResource("../Seogu/gc.PNG").getPath();
-   String pathgho = this.getClass().getResource("../Seogu/gho.PNG").getPath();
-   String pathnb = this.getClass().getResource("../Seogu/nb.PNG").getPath();
-   String pathns = this.getClass().getResource("../Seogu/ns.PNG").getPath();
-   String pathdh = this.getClass().getResource("../Seogu/dh.PNG").getPath();
-   String pathdc = this.getClass().getResource("../Seogu/dc.PNG").getPath();
-   String pathmr = this.getClass().getResource("../Seogu/mr.PNG").getPath();
-   String pathmelw = this.getClass().getResource("../Seogu/melw.PNG").getPath();
-   String pathbj = this.getClass().getResource("../Seogu/bj.PNG").getPath();
-   String pathsc = this.getClass().getResource("../Seogu/sc.PNG").getPath();
-   String pathsha = this.getClass().getResource("../Seogu/sha.PNG").getPath();
-   String pathssangc = this.getClass().getResource("../Seogu/ssangc.PNG").getPath();
-   String pathyang = this.getClass().getResource("../Seogu/yang.PNG").getPath();
-   String pathydo = this.getClass().getResource("../Seogu/ydo.PNG").getPath();
-   String pathuch = this.getClass().getResource("../Seogu/uch.PNG").getPath();
-   String pathcci = this.getClass().getResource("../Seogu/cci.PNG").getPath();
-   String pathqa = this.getClass().getResource("../Seogu/qa.PNG").getPath();
-   String pathhj = this.getClass().getResource("../Seogu/hj.PNG").getPath();
-      
-   
-   ImageIcon[] images = { new ImageIcon(pathgc), new ImageIcon(pathgho),
-         new ImageIcon(pathnb), new ImageIcon(pathns), new ImageIcon(pathdh),
-         new ImageIcon(pathdc), new ImageIcon(pathmr), new ImageIcon(pathmelw),
-         new ImageIcon(pathbj), new ImageIcon(pathsc), new ImageIcon(pathsha),
-         new ImageIcon(pathssangc), new ImageIcon(pathyang), new ImageIcon(pathydo),
-         new ImageIcon(pathuch), new ImageIcon(pathcci), new ImageIcon(pathqa),
-         new ImageIcon(pathhj),};
+	Container contentPane;
+	String[] gu = { "광천동", "금호동", "내방동", "농성동", "덕흥동", "동천동", "마륵동", "매월동", "벽진동", "서창동", "세하동", "쌍촌동", "양동", "용두동",
+			"유촌동", "치평동", "풍암동", "화정동" };
 
-   JLabel imgLabel = new JLabel(images[0]);
+	String pathgc = this.getClass().getResource("../Seogu/gc.PNG").getPath();
+	String pathgho = this.getClass().getResource("../Seogu/gho.PNG").getPath();
+	String pathnb = this.getClass().getResource("../Seogu/nb.PNG").getPath();
+	String pathns = this.getClass().getResource("../Seogu/ns.PNG").getPath();
+	String pathdh = this.getClass().getResource("../Seogu/dh.PNG").getPath();
+	String pathdc = this.getClass().getResource("../Seogu/dc.PNG").getPath();
+	String pathmr = this.getClass().getResource("../Seogu/mr.PNG").getPath();
+	String pathmelw = this.getClass().getResource("../Seogu/melw.PNG").getPath();
+	String pathbj = this.getClass().getResource("../Seogu/bj.PNG").getPath();
+	String pathsc = this.getClass().getResource("../Seogu/sc.PNG").getPath();
+	String pathsha = this.getClass().getResource("../Seogu/sha.PNG").getPath();
+	String pathssangc = this.getClass().getResource("../Seogu/ssangc.PNG").getPath();
+	String pathyang = this.getClass().getResource("../Seogu/yang.PNG").getPath();
+	String pathydo = this.getClass().getResource("../Seogu/ydo.PNG").getPath();
+	String pathuch = this.getClass().getResource("../Seogu/uch.PNG").getPath();
+	String pathcci = this.getClass().getResource("../Seogu/cci.PNG").getPath();
+	String pathqa = this.getClass().getResource("../Seogu/qa.PNG").getPath();
+	String pathhj = this.getClass().getResource("../Seogu/hj.PNG").getPath();
 
-   Seogu() {
+	ImageIcon[] images = { new ImageIcon(pathgc), new ImageIcon(pathgho), new ImageIcon(pathnb), new ImageIcon(pathns),
+			new ImageIcon(pathdh), new ImageIcon(pathdc), new ImageIcon(pathmr), new ImageIcon(pathmelw),
+			new ImageIcon(pathbj), new ImageIcon(pathsc), new ImageIcon(pathsha), new ImageIcon(pathssangc),
+			new ImageIcon(pathyang), new ImageIcon(pathydo), new ImageIcon(pathuch), new ImageIcon(pathcci),
+			new ImageIcon(pathqa), new ImageIcon(pathhj), };
 
-      setTitle("ComboBox");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      contentPane = getContentPane();
-      contentPane.setLayout(new FlowLayout());
+	JLabel imgLabel = new JLabel(images[0]);
 
-      JComboBox strCombo = new JComboBox(gu);
+	Seogu() {
 
-      strCombo.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-            JComboBox cb = (JComboBox) e.getSource();
+		setTitle("ComboBox");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = getContentPane();
 
-            int index = cb.getSelectedIndex();
+		JComboBox strCombo = new JComboBox(gu);
+		strCombo.setMaximumRowCount(15);
+		strCombo.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		strCombo.setBounds(31, 206, 130, 30);
 
-            imgLabel.setIcon(images[index]);
-         }
-      });
+		strCombo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JComboBox cb = (JComboBox) e.getSource();
 
-      contentPane.add(strCombo);
-      contentPane.add(imgLabel);
+				int index = cb.getSelectedIndex();
 
-      setSize(1017, 711);
-      setVisible(true);
+				imgLabel.setIcon(images[index]);
+			}
+		});
+		getContentPane().setLayout(null);
 
-   }
+		contentPane.add(strCombo);
+		imgLabel.setBounds(197, 153, 964, 581);
+		contentPane.add(imgLabel);
 
-   public static void main(String arg[]) {
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 0, 1194, 761);
+		getContentPane().add(panel);
 
-      new Seogu();
-   }
+		URL url = this.getClass().getResource("../GUI_Image/seogu_map.jpg");
+		panel.setLayout(null);
+		JLabel login_background = new JLabel(new ImageIcon(url.getPath()));
+		login_background.setBounds(0, 0, 1200, 761);
+		panel.add(login_background);
+
+		setSize(1210, 800);
+		setVisible(true);
+
+	}
+
+	public static void main(String arg[]) {
+
+		new Seogu();
+	}
 }
